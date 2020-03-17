1fe5ad10fa74f0d664488e6516cf6525b9c93997
andrey-posmitniy <andrey.posmitniy@mail.ru>
2020-03-17 11:52:41 +0300

    Add files via upload

---
+++ mvc/Vm/Utils/Ioc.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using SimpleInjector;
+
+namespace Vm.Utils
+{
+    /// <summary>
+    /// Содержит ссылку на основной IoC-контейнер приложения
+    /// </summary>
+    public class IoC
+    {
+        /// <summary>
+        /// Основной IoC-контейнер приложения
+        /// </summary>
+        public static SimpleInjector.Container Container { get; set; }
+    }
+}
